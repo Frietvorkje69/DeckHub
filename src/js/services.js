222f35cfc3b56a763c1e56df6e1a4280ce2af50c
alxspiker <alxtheprogrammer@gmail.com>
2024-04-11 09:23:40 -0600

    Better wording

--- src/js/services.js
+++ src/js/services.js
@@ -3,7 +3,7 @@ const services = [
         "name": "Netflix",
         "link": "https://www.netflix.com/",
         "image": "https://bramptonist.com/wp-content/uploads/2018/06/netflix-image.jpg",
-        "status": "legal",
+        "status": "Official",
         "category": "Streaming",
         "id": 1
     },
@@ -11,7 +11,7 @@ const services = [
         "name": "Prime Video",
         "link": "https://www.primevideo.com/",
         "image": "https://th.bing.com/th/id/OIP.7WRaKoAz7CXJ5uHczOx2DwAAAA?rs=1&pid=ImgDetMain",
-        "status": "legal",
+        "status": "Official",
         "category": "Streaming",
         "id": 2
     },
@@ -19,14 +19,14 @@ const services = [
         "name": "Disney+",
         "link": "https://www.disneyplus.com/",
         "image": "https://i.pinimg.com/736x/f1/ea/4b/f1ea4b62940f2ca19b21f6bdfcc1eec4.jpg",
-        "status": "legal",
+        "status": "Official",
         "category": "Streaming"
     },
     {
         "name": "Hulu",
         "link": "https://www.hulu.com/",
         "image": "https://wallpapercave.com/wp/wp2491671.png",
-        "status": "legal",
+        "status": "Official",
         "category": "Streaming",
         "id": 3
     },
@@ -34,7 +34,7 @@ const services = [
         "name": "HBO Max",
         "link": "https://www.hbomax.com/",
         "image": "https://www.iphon.fr/app/uploads/2019/11/hbo-max-logo.png",
-        "status": "legal",
+        "status": "Official",
         "category": "Streaming",
         "id": 4
     },
@@ -42,7 +42,7 @@ const services = [
         "name": "Paramount+",
         "link": "https://www.paramountplus.com/",
         "image": "https://www.hollywoodreporter.com/wp-content/uploads/2011/06/paramount_logo_2011_a_l.jpg?w=2000&h=1126&crop=1",
-        "status": "legal",
+        "status": "Official",
         "category": "Streaming",
         "id": 5
     },
@@ -50,7 +50,7 @@ const services = [
         "name": "Crunchyroll",
         "link": "https://www.crunchyroll.com/",
         "image": "https://assets.aboutamazon.com/dims4/default/dc6e6ef/2147483647/strip/false/crop/1920x1080+0+0/resize/1200x675!/quality/90/?url=https%3A%2F%2Famazon-blogs-brightspot.s3.amazonaws.com%2Ff8%2F2a%2Fe21459814491b9aac0134bfffaf8%2Fcr-1920x1080.jpg",
-        "status": "legal",
+        "status": "Official",
         "category": "Anime",
         "id": 6
     },
@@ -58,7 +58,7 @@ const services = [
         "name": "AniWave",
         "link": "https://aniwave.to/home",
         "image": "https://cdn2.steamgriddb.com/thumb/1db6c162d8331ec63d1ca1975bff2c1d.jpg",
-        "status": "illegal",
+        "status": "Unofficial",
         "category": "Anime",
         "id": 7
     },
@@ -66,7 +66,7 @@ const services = [
         "name": "HiAnime",
         "link": "https://hianime.to/home",
         "image": "https://www.slashfilm.com/img/gallery/the-entire-attack-on-titan-timeline-explained/intro-1656893069.jpg",
-        "status": "illegal",
+        "status": "Unofficial",
         "category": "Anime",
         "id": 8
     },
@@ -74,7 +74,7 @@ const services = [
         "name": "Animeflix",
         "link": "https://animeflix.live",
         "image": "https://sp.rmbl.ws/s8/1/j/S/6/2/jS62b.qR4e-small-ANIMEFLIX-GREAT-FREE-ANIME-.jpg",
-        "status": "illegal",
+        "status": "Unofficial",
         "category": "Anime",
         "id": 9
     },
@@ -82,7 +82,7 @@ const services = [
         "name": "Animension",
         "link": "https://www.animension.to/",
         "image": "https://m.media-amazon.com/images/M/MV5BYWE1OTM1ZDktNWNiOS00YzM0LTkxNzQtYjE3YTYzY2NjYzIyXkEyXkFqcGdeQXVyNzI3NjY3NjQ@._V1_.jpg",
-        "status": "illegal",
+        "status": "Unofficial",
         "category": "Anime",
         "id": 10
     },
@@ -90,7 +90,7 @@ const services = [
         "name": "YouTube",
         "link": "https://www.youtube.com/",
         "image": "https://cdn2.steamgriddb.com/hero_thumb/4a9f57c5dadf5bc6555a2e754ca3cfa7.jpg",
-        "status": "legal",
+        "status": "Official",
         "category": "Social",
         "id": 11
     },
@@ -98,7 +98,7 @@ const services = [
         "name": "Twitch",
         "link": "https://www.twitch.tv/",
         "image": "https://wallpaper.dog/large/20477304.png",
-        "status": "legal",
+        "status": "Official",
         "category": "Social",
         "id": 12
     },
@@ -106,7 +106,7 @@ const services = [
         "name": "MoviesJoy",
         "link": "https://moviesjoy.is/home",
         "image": "https://play-lh.googleusercontent.com/FwNsrfV0FUCkDw2BKUDyewEE1zPbzq0VCf1xJmxAMnYCDMKxlgknjDfl_0OHOOVum4o",
-        "status": "illegal",
+        "status": "Unofficial",
         "category": "Movies",
         "id": 13
     },
@@ -114,7 +114,7 @@ const services = [
         "name": "Movies7",
         "link": "https://movies7.to/home",
         "image": "https://editorialge.com/wp-content/uploads/2023/05/Movies7.jpeg",
-        "status": "illegal",
+        "status": "Unofficial",
         "category": "Movies",
         "id": 14
     }
